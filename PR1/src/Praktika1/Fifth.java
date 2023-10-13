e62c66767448a281c90ffcd25bc6c7966b0dbf2a
Artemma07 <101255236+Artemma07@users.noreply.github.com>
2023-10-13 17:38:22 +0300

    Add files via upload

---
+++ PR1/src/Praktika1/Fifth.java
@@ -0,0 +1,15 @@
+package Praktika1;
+
+public class Fifth {
+    static int calculateFactorial(int n){
+        int result = 1;
+        for (int i = 1; i <=n; i ++){
+            result = result*i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args){
+        System.out.println(calculateFactorial(4));
+    }
+}
