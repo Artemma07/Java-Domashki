a9c96696f645ef22e6bca60e8f8de0a48ed25e30
Artemma07 <101255236+Artemma07@users.noreply.github.com>
2023-10-18 17:41:36 +0300

    Add files via upload

---
+++ PR2/src/Пр1/TestAuthor.java
@@ -0,0 +1,8 @@
+package Пр1;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a = new Author("Артём", "artem@mail.ru", 'm');
+        System.out.println(a);
+    }
+}
